72d52063c02611edf228ac6b8b2e5538935af8ba
Akhayat <akhayat21@yahoo.com>
2019-03-08 14:21:03 -0500

    javascript edit

--- javascript.js
+++ javascript.js
@@ -1,15 +1,20 @@
 var searchTerm;
 var beginDate;
 var endDate;
+var queryResults = [];
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchTerm+"&begin_date="+beginDate"&end_date="+endDate+"&api-key=TnjsUG68GH5qaLNjiIEP84RB7OLNMZS5"
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchTerm+"&begin_date="+beginDate+"&end_date="+endDate+"&api-key=TnjsUG68GH5qaLNjiIEP84RB7OLNMZS5"
 //ajax call with get method 
       $.ajax({
         url: queryURL,
         method: "GET"
-      })
+      }).then(function(response){
+        for(i=0; i<10;i++){
+            var headline = response.headline[i];
+        }
+      });
       
       
       
       
-     for (var i = 0; i < results.length; i++) {
+    
