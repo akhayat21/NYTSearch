bcd16e49834dc145a57ea7577df7dd8f9885f190
Akhayat <akhayat21@yahoo.com>
2019-03-08 14:26:09 -0500

    fixed rsponse

--- javascript.js
+++ javascript.js
@@ -3,18 +3,20 @@ var beginDate;
 var endDate;
 var queryResults = [];
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchTerm+"&begin_date="+beginDate+"&end_date="+endDate+"&api-key=TnjsUG68GH5qaLNjiIEP84RB7OLNMZS5"
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=donaldtrump&api-key=TnjsUG68GH5qaLNjiIEP84RB7OLNMZS5"
 //ajax call with get method 
       $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function(response){
-        for(i=0; i<10;i++){
-            var headline = response.headline[i];
-        }
+      }).then( function(response){
+
+        console.log(response);
+        // for(i=0; i<10;i++){
+        //     var headline = response.headline[i];
+        // }
       });
       
       
       
-      
+      //&begin_date="+beginDate+"&end_date="+endDate+
     
